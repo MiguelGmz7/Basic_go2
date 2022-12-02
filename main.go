a1dab9c3042c0faaf8e871b350e193889ec41745
MiguelGmz7 <oscar.gomez5178@alumnos.udg.mx>
2022-12-02 02:42:47 -0600

    po

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+/*
 package main 
 
 import ( 
@@ -20,13 +21,36 @@ func EncodeManager(manager *Manager) (io.Reader, error) {
 	}
 	fmt.Println(string(e))
 }*/
-
+/*
 func main () {
 	man := &Manager{Fullname: "Rocky", Position: "Tier1", Age: 51, YearsInCompany: 5}
     e, err := json.Marshal(man)
     if e, err != nil {
         fmt.Println(err)
        // return
+    }
+    fmt.Println(string(e))
+}
+*/
+
+package main
+
+import (
+    "fmt"
+    "encoding/json"
+)
+
+type Employee struct {
+    Name string `json:"empname"`
+	Number int  `json:"empid"`
+}
+
+func main() {
+    emp := &Employee{Name: "Rocky",Number: 5454}
+    e, err := json.Marshal(emp)
+    if err != nil {
+        fmt.Println(err)
+        return
     }
     fmt.Println(string(e))
 }
