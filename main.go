a8868517b25ad2614743273e161329b825a8f83e
MiguelGmz7 <oscar.gomez5178@alumnos.udg.mx>
2022-12-02 02:46:38 -0600

    jo

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-/*
+
 package main 
 
 import ( 
@@ -12,27 +12,21 @@ type Manager struct {
 	Age int32
 	YearsInCompany int32
 }
-/*
-func EncodeManager(manager *Manager) (io.Reader, error) {
+
+func EncodeManager(manager *Manager) {
 	e, err := json.Marshal(manager)
 	if e, err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Println(string(e))
-}*/
-/*
+}
 func main () {
 	man := &Manager{Fullname: "Rocky", Position: "Tier1", Age: 51, YearsInCompany: 5}
-    e, err := json.Marshal(man)
-    if e, err != nil {
-        fmt.Println(err)
-       // return
-    }
-    fmt.Println(string(e))
+    EncodeManager(man)
 }
-*/
 
+/*
 package main
 
 import (
@@ -54,3 +48,4 @@ func main() {
     }
     fmt.Println(string(e))
 }
+*/
