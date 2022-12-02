fb3ac6f225685b8949624d4a6eca0bc500d33735
MiguelGmz7 <oscar.gomez5178@alumnos.udg.mx>
2022-12-02 02:13:31 -0600

    si

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main 
 
-import "fmt"
+import ( 
+	"fmt"
+	"encoding/json"
+)
 
 type Manager struct {
 	Fullname string
@@ -9,6 +12,15 @@ type Manager struct {
 	YearsInCompany int32
 }
 
+func EncodeManager(manager *Manager) (io.Reader, error) {
+	err := json.Marshal(manager)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
 func main () {
-	
+	man := &Manager{Fullname: "Rocky", Position: "Tier1", Age: 51, YearsInCompany: 5}
+	EncodeManager(man)
 }
