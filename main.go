3fb57a9f17a722b6a033ff5af4ae1d602b3bbb31
MiguelGmz7 <oscar.gomez5178@alumnos.udg.mx>
2022-12-02 03:16:13 -0600

    ko

--- main.go
+++ main.go
@@ -7,14 +7,14 @@ import (
 )
 
 type Manager struct {
-	FullName string 'json:"full_name"'
-	Position string 'json: "position"'
-	Age int32 'json: "age"'
-	YearsInCompany 'json: "years_in_company"'
+	FullName string  `json:"full_name"`
+	Position string `json: "position"`
+	Age int32 `json: "age"`
+	YearsInCompany int32 `json: "years_in_company"`
 }
 
 func main() {
-    emp := &Manager{FullName: "Jack Oliver", Position: "CEO", Age: "44", YearsInCompany: "8"}
+    emp := &Manager{FullName: "Jack Oliver", Position: "CEO", Age: 44, YearsInCompany: 8}
     e, err := json.Marshal(emp)
     if err != nil {
         fmt.Println(err)
