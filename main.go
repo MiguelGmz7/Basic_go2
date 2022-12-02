e064138fe1ba347017e0634a2374d2c75fd3bf67
MiguelGmz7 <oscar.gomez5178@alumnos.udg.mx>
2022-12-02 03:07:45 -0600

    fin

--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import (
     "encoding/json"
 )
 
-type Employee struct {
-    Name string `json:"empname"`
-	Number int  `json:"empid"`
+type Manager struct {
+	FullName string 'json:"full_name"'
+	Position string 'json: "position"'
+	Age int32 'json: "age"'
+	YearsInCompany 'json: "years_in_company"'
 }
 
 func main() {
-    emp := &Employee{Name: "Rocky",Number: 5454}
+    emp := &Manager{FullName: "Jack Oliver", Position: "CEO", Age: "44", YearsInCompany: "8"}
     e, err := json.Marshal(emp)
     if err != nil {
         fmt.Println(err)
